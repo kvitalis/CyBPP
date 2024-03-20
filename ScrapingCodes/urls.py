07a0f04cfd000ca96512059a9823ccbf32155809
IoannisHadjimichaelCY <157680574+IoannisHadjimichaelCY@users.noreply.github.com>
2024-03-20 10:25:13 +0200

    Update urls.py

--- ScrapingCodes/urls.py
+++ ScrapingCodes/urls.py
@@ -5,16 +5,20 @@ import requests
 file_path = 'StoredScrapedData/Product_urls.xlsx'  # Update this to the path of your Excel file
 df = pd.read_excel(file_path)
 
-# Function to check URL accessibility
 def check_url(url):
-    try:
-        response = requests.head(url, allow_redirects=True, timeout=5)
-        if response.status_code == 200:
-            return 'CONFIRMED'
-        else:
-            return 'ERROR'
-    except requests.RequestException:
-        return 'ERROR'
+    methods = [requests.head, requests.get]  # List of methods to try
+    for method in methods:
+        try:
+            # Try the current method
+            response = method(url, allow_redirects=True, timeout=10)
+            # If the response code is 200, the URL is accessible
+            if response.status_code == 200:
+                return 'CONFIRMED'
+        except requests.RequestException:
+            # If there's an exception, log it or pass
+            pass
+    # If all methods failed, return 'ERROR'
+    return 'ERROR'
 
 # Check each URL and write the result
 df['Confirmed'] = df['Item.url'].apply(check_url)  # Update 'URL_Column_Name' to your actual URL column name
