f108dc119646bb35109d3a532108eb1459314673
kmicha714 <159122641+kmicha714@users.noreply.github.com>
2024-04-03 15:08:42 +0300

    Update ScrapingCode.py

--- ScrapingCodes/ScrapingCode.py
+++ ScrapingCodes/ScrapingCode.py
@@ -1998,7 +1998,7 @@ for u in range(0,len(urls)):
     subclass_=urls["Subclass"].iloc[u]
     comitidy_=urls["Division"].iloc[u]
     retailer_=urls["Retailer"].iloc[u]
-    print(Item_url)
+    print(Item_url_)
     
 
     if retailer_=="SupermarketCy":
