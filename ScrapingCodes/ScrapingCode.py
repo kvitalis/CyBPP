dd538845f91a513ad15e8f701bb27e0b1dad6653
kmicha714 <159122641+kmicha714@users.noreply.github.com>
2024-04-19 12:33:52 +0300

    Update ScrapingCode.py

--- ScrapingCodes/ScrapingCode.py
+++ ScrapingCodes/ScrapingCode.py
@@ -963,7 +963,7 @@ def resutls_rio(u):
                         new_row.append(float(match.group(2)))
                         new_row.append(subclass_)
                         new_row.append(comitidy_)
-                        new_row.append("Primetel")
+                        new_row.append("Rio Cinema")
                         list_.loc[len(list_)] = new_row
                         list_['Name'] = list_['Name'].apply(lambda x:x)
                     else:
@@ -986,7 +986,7 @@ def resutls_rio(u):
                         new_row.append(float(price_))
                         new_row.append(subclass_)
                         new_row.append(comitidy_)
-                        new_row.append("Primetel")
+                        new_row.append("Rio Cinema")
                         list_.loc[len(list_)] = new_row
                         list_['Name'] = list_['Name'].apply(lambda x:x)
                     else:
