6b421651480a0ea80c335b2682b733efd42e2908
kmicha714 <159122641+kmicha714@users.noreply.github.com>
2024-04-23 12:58:44 +0300

    Update Calculations.py

--- PythonCodes/Calculations.py
+++ PythonCodes/Calculations.py
@@ -1,7 +1,9 @@
 # Important Libraries
 import pandas as pd 
-from datetime import datetime
 import warnings
+import matplotlib.pyplot as plt
+
+from datetime import datetime
 from datetime import datetime, timedelta
 
 # Ignore specific warning
@@ -144,3 +146,28 @@ df_105 = pd.DataFrame([new_row], columns=['Date', 'CPI General', 'Inflation (%)'
 df_106= pd.concat([df_104, df_105],ignore_index=True)
 df_106['Inflation (%)']= 100*(df_106['CPI General'] - df_106['CPI General'].shift(1)) / df_106['CPI General'].shift(1)
 df_106.to_csv("Results/CPI-General-Inflation.csv", index=False)
+
+##EVRY LAST THURSDAY
+#Current date
+current_date = datetime.now().strftime("%Y-%m-%d")
+
+#Read importnat files
+df_=pd.read_csv("Results/CPI-General-Inflation.csv")
+df_montly_data=pd.read_csv("Results Monthly/CPI-General-Inflation-Monthly.csv")
+
+#Fuction's calculation of last Thursday
+def is_last_thursday(date):
+    date = datetime.strptime(date, "%Y-%m-%d")
+    weekday = date.weekday()
+    if weekday == 3 and date.month != (date + timedelta(days=7)).month:
+        return True
+    return False
+
+#Call the function
+if is_last_thursday(current_date):
+    df_current_date=df_[df_["Date"]==current_date]
+    df_montly_data = df_montly_data.append(df_current_date, ignore_index=True)
+    df_montly_data["Inflation (%)"]=None
+    df_montly_data.to_csv("Results Monthly/CPI-General-Inflation-Monthly.csv")
+else:
+    pass
