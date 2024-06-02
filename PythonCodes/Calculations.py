cb6c5e55cb203fa6fa352666b82a44c5657662cd
kmicha714 <159122641+kmicha714@users.noreply.github.com>
2024-06-02 03:02:04 +0300

    Update Calculations.py

--- PythonCodes/Calculations.py
+++ PythonCodes/Calculations.py
@@ -9,8 +9,8 @@ from datetime import datetime, timedelta
 # Ignore specific warning
 warnings.simplefilter("ignore")
 
-today=datetime.today().strftime("%Y-%m-%d")
-#today='2024-05-26'
+#today=datetime.today().strftime("%Y-%m-%d")
+today='2024-06-01'
 
 #CALCULATIONS
 
