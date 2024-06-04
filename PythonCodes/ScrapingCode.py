ebf5335a59e130748050090165c7572a3fd0191d
kmicha714 <159122641+kmicha714@users.noreply.github.com>
2024-06-04 16:46:59 +0300

    Update ScrapingCode.py

--- PythonCodes/ScrapingCode.py
+++ PythonCodes/ScrapingCode.py
@@ -1578,14 +1578,21 @@ def resutls_toyta(u):
                 daily_errors["Name"] =daily_errors["Name"].apply(lambda x:x)
         
         elif subclass_=="Second-hand motor cars":
-            query ={"component":"used-stock-cars-v2","fetches":[
-                {"fetchType":"fetchUscVehiclePrice","vehicleForSaleId":"4077c595-5c2c-42bd-8133-203d770ad125","context":"used","uscEnv":"production"}
-            ]}
-            headers = {"Host": "usc-webcomponents.toyota-europe.com","User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:126.0) Gecko/20100101 Firefox/126.0","Accept": "*/*","Accept-Language": "en-US,en;q=0.5","Accept-Encoding": "gzip, deflate, br, zstd","Content-Type": "application/json","Content-Length": "180","Origin": "https://www.toyota.com.cy","Connection": "keep-alive","Referer": "https://www.toyota.com.cy/","Sec-Fetch-Dest": "empty","Sec-Fetch-Mode": "cors","Sec-Fetch-Site": "cross-site","Priority": "u=6","TE": "trailers"
-            }
-            response = requests.get(Item_url_,{'headers':headers})
-            r = requests.post("https://usc-webcomponents.toyota-europe.com/v1/api/data/cy/en?brand=toyota&uscEnv=production", json=query, headers=headers)
-            price_=r.json()['fetches'][0]['result']['fetchResult'] ['sellingPriceInclVAT']
+            #query ={"component":"used-stock-cars-v2","fetches":[
+            #    {"fetchType":"fetchUscVehiclePrice","vehicleForSaleId":"4077c595-5c2c-42bd-8133-203d770ad125","context":"used","uscEnv":"production"}
+            #]}
+            #headers = {"Host": "usc-webcomponents.toyota-europe.com","User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:126.0) Gecko/20100101 Firefox/126.0","Accept": "*/*","Accept-Language": "en-US,en;q=0.5","Accept-Encoding": "gzip, deflate, br, zstd","Content-Type": "application/json","Content-Length": "180","Origin": "https://www.toyota.com.cy","Connection": "keep-alive","Referer": "https://www.toyota.com.cy/","Sec-Fetch-Dest": "empty","Sec-Fetch-Mode": "cors","Sec-Fetch-Site": "cross-site","Priority": "u=6","TE": "trailers"
+            #}
+            #response = requests.get(Item_url_,{'headers':headers})
+            #r = requests.post("https://usc-webcomponents.toyota-europe.com/v1/api/data/cy/en?brand=toyota&uscEnv=production", json=query, headers=headers)
+            #price_=r.json()['fetches'][0]['result']['fetchResult'] ['sellingPriceInclVAT']
+
+            bs = BeautifulSoup(Item_url_, "html.parser")
+            response = requests.get(bs)
+            soup = BeautifulSoup(response.content, "html.parser")
+            ken = soup.find("div", {"role": "cpdqm_ignore"}).text
+            data = json.loads(ken)
+            price_ = data['vehicle']['result']['price']['sellingPriceInclVAT']
 
             if price_:
                 new_row.append(datetime.now().strftime('%Y-%m-%d'))
