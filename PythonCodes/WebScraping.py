9f95280c0c44eb2b99377f1e7d2f4caa2afd5a16
kmicha714 <159122641+kmicha714@users.noreply.github.com>
2024-06-07 16:30:52 +0300

    Update WebScraping.py

--- PythonCodes/WebScraping.py
+++ PythonCodes/WebScraping.py
@@ -553,7 +553,7 @@ def results_CyMinistryEducation(u):
 
         for i in range(2,7):
             pdf_[i] = pdf_[i].astype('string')
-            if subclass_="Secondary education":
+            if subclass_=="Secondary education":
                 value_1=(float(pdf_[2][4].replace("€",'').replace(".","")))
                 value_2=(float(pdf_[3][4].replace("€",'').replace(".","")))
                 value_3=(float(pdf_[4][4].replace("€",'').replace(".","")))
@@ -562,7 +562,7 @@ def results_CyMinistryEducation(u):
                 value_6=(float(pdf_[7][4].replace("€",'').replace(".","")))
                 price_ = float(value_1 + value_2 + value_3 + value_4 + value_5 + value_6) / 6
 
-            if subclass_="Post-secondary non-tertiary education (ISCED 4)":
+            if subclass_=="Post-secondary non-tertiary education (ISCED 4)":
                 pdf_[8] = pdf_[8].astype('string')
                 value_7 = (float(pdf_[8][4].replace("€",'').replace(".",""))) 
                 price_ = float(value_7)
@@ -573,7 +573,7 @@ def results_CyMinistryEducation(u):
         
         for i in range(2,7):
             pdf_[i] = pdf_[i].astype('string')
-            if subclass_="Secondary education":
+            if subclass_=="Secondary education":
                 value_1=(float(pdf_[2][15].replace("€",'').replace(".","")))
                 value_2=(float(pdf_[3][15].replace("€",'').replace(".","")))
                 value_3=(float(pdf_[4][15].replace("€",'').replace(".","")))
@@ -582,7 +582,7 @@ def results_CyMinistryEducation(u):
                 value_6=(float(pdf_[7][15].replace("€",'').replace(".","")))
                 price_ = float(value_1 + value_2 + value_3 + value_4 + value_5 + value_6) / 6
 
-            if subclass_="Post-secondary non-tertiary education (ISCED 4)":
+            if subclass_=="Post-secondary non-tertiary education (ISCED 4)":
                 pdf_[8] = pdf_[8].astype('string')
                 value_7 = (float(pdf_[8][15].replace("€",'').replace(".",""))) 
                 price_ = float(value_7)
