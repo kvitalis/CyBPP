0cf64c266e97b06b8f5db5637a2a8fc72784b53c
kvitalis <157591960+kvitalis@users.noreply.github.com>
2024-09-27 12:26:04 +0300

    Update Visualizations.py

--- PythonCodes/Visualizations.py
+++ PythonCodes/Visualizations.py
@@ -43,9 +43,7 @@ plt.show()
 #========================================================================================================================
 
 #Current date
-#today_date = datetime.strptime(today, "%Y-%m-%d")
-current_date ='2024-09-26'
-#current_date = today_date.strftime("%Y-%m-%d")
+current_date = today_date.strftime("%Y-%m-%d")
 
 #Read data
 df_monthly = pd.read_csv("Results/Monthly-CPI-General-Inflation.csv")
