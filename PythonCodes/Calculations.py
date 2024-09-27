702f1e24a3cb77d0837772ce4205f2e06205a0ab
kmicha714 <159122641+kmicha714@users.noreply.github.com>
2024-09-27 16:04:44 +0300

    Update Calculations.py

--- PythonCodes/Calculations.py
+++ PythonCodes/Calculations.py
@@ -10,7 +10,7 @@ from datetime import datetime, timedelta
 warnings.simplefilter("ignore")
 
 #today=datetime.today().strftime("%Y-%m-%d")
-today='2024-09-25'
+today='2024-09-26'
 
 #Read necessary data 
 raw_data = pd.read_csv("Datasets/Raw-Data.csv", parse_dates=['Date'], date_parser=lambda x:pd.to_datetime(x, format='%Y-%m-%d'))
