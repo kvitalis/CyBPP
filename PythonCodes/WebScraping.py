81877319b39e6d600b62a00fcbb3a18462b1a2e0
kvitalis <157591960+kvitalis@users.noreply.github.com>
2024-10-19 20:27:33 +0300

    Update WebScraping.py

--- PythonCodes/WebScraping.py
+++ PythonCodes/WebScraping.py
@@ -1091,9 +1091,9 @@ def results_rio(u):
                         daily_errors["Name"] =daily_errors["Name"].apply(lambda x:x)
 
 def results_AHK(u):
-    #pdf_AHK = "PDFs/AHK_Mar2024.pdf"
-    """
+    pdf_AHK = "PDFs/AHK_Mar2024.pdf"
     response = requests.get(Item_url_)
+    
     if response.status_code !=200:
         website_false.append(name_)
         website_false.append(subclass_)
@@ -1105,8 +1105,6 @@ def results_AHK(u):
     else:
         with open(pdf_AHK, "wb") as f:
             f.write(response.content)
-        """
-        pdf_AHK = "PDFs/AHK_Mar2024.pdf"
         with open(pdf_AHK, "rb") as f:
             #pdf_reader = PyPDF2.PdfReader(f)
             pdf_reader = pypdf.PdfReader(f)
