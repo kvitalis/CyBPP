93dfedc71c78bc577c4a4fd4ade06cae3825cad9
kvitalis <157591960+kvitalis@users.noreply.github.com>
2024-11-26 10:56:34 +0200

    Update WebScraping.py

--- PythonCodes/WebScraping.py
+++ PythonCodes/WebScraping.py
@@ -1856,7 +1856,7 @@ def results_toyota(u):
             website_false.append(name_)
             website_false.append(subclass_)
             website_false.append(Item_url_)
-            website_false.append(commodity_)
+            website_false.append(division_)
             website_false.append(retailer_)
             daily_errors.loc[len(daily_errors)] = website_false
             daily_errors["Name"] = daily_errors["Name"].apply(lambda x:x)
@@ -1879,7 +1879,7 @@ def results_toyota(u):
             new_row.append(name_)
             new_row.append(price_) 
             new_row.append(subclass_)
-            new_row.append(commodity_)
+            new_row.append(division_)
             new_row.append("Toyota")
             list_.loc[len(list_)] = new_row
             list_['Name'] = list_['Name'].apply(lambda x:x)
