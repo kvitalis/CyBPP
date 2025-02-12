3ade11d8ebc6483aba63a98e6c7c54b5c3fb5a25
kmicha714 <159122641+kmicha714@users.noreply.github.com>
2025-02-12 14:55:07 +0200

    Update CyFuelsPrices_PriceStatisticsCalculations.py

--- CyFuelsPrices/CyFuelsPrices_PriceStatisticsCalculations.py
+++ CyFuelsPrices/CyFuelsPrices_PriceStatisticsCalculations.py
@@ -30,7 +30,12 @@ warnings.simplefilter("ignore")
 df_data = pd.read_csv("CyFuelsPrices/CyFuelsPrices_ScrapedData.csv")
 df_stats = pd.read_csv("CyFuelsPrices/CyFuelsPrices_PriceStatistics.csv")
 
-fuel_group = df_data.groupby("Fuel Type").mean()
-average_price = fuel_group["Price"].mean()
-print(average_price)
+df_12=df_data[df_data["Date"]=="2025-02-11"]
+df_23 = df_12[df_12["Fuel Type"] == "Unleaded 95"]
+average_price_value_23=df_23.mean()
+print(average_price_value_23)
+
+#fuel_group = df_data.groupby("Fuel Type").mean()
+#average_price = fuel_group["Price"].mean()
+#print(average_price)
 #df_stats.loc[len(df_stats)]=[,,,,,]
