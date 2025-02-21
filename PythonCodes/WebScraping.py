1acd552477ff39865cf7fbb9f030f08a031afe4d
kvitalis <157591960+kvitalis@users.noreply.github.com>
2025-02-21 10:41:52 +0200

    Update WebScraping.py

--- PythonCodes/WebScraping.py
+++ PythonCodes/WebScraping.py
@@ -2697,8 +2697,8 @@ for u in range(0, len(urls)):
         results_parga(u)    
     elif retailer_=="leroymerlin":
         results_leroymerlin(u)   
-    #elif retailer_=="IKEA":
-        #results_IKEA(u)
+    elif retailer_=="IKEA":
+        results_IKEA(u)
     #elif retailer_=="Stephanis":
         #results_stephanis(u)
     elif retailer_=="Electroline":
