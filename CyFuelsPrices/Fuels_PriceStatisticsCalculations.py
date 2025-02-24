6aad34d232a5f1d7fd78e19d064d9ea0e9735d96
kvitalis <157591960+kvitalis@users.noreply.github.com>
2025-02-24 14:05:51 +0200

    Update Fuels_PriceStatisticsCalculations.py

--- CyFuelsPrices/Fuels_PriceStatisticsCalculations.py
+++ CyFuelsPrices/Fuels_PriceStatisticsCalculations.py
@@ -38,35 +38,85 @@ print(today)
 df_date = df_data[df_data["Date"] == today]
 new_row = []
 
-## Fuels Prices Statistics
+### Fuels Prices Statistics
 
-#Unleaded 95
+## Unleaded 95
 df_type = df_date[df_date["Fuel Type"] == "Unleaded 95"]
+#Mean/Average
 unleaded95_avg = round(df_type["Price"].mean(),3)
 print(unleaded95_avg)
+#Minimum
+unleaded95_min = round(df_type["Price"].min(),3)
+print(unleaded95_min)
+#Maximum
+unleaded95_max = round(df_type["Price"].max(),3)
+print(unleaded95_max)
+#Median/2nd Quartile
+unleaded95_q2 = round(df_type["Price"].median(),3)
+print(unleaded95_q2)
 
-#Unleaded 95
+## Unleaded 98
 df_type = df_date[df_date["Fuel Type"] == "Unleaded 98"]
+#Mean/Average
 unleaded98_avg = round(df_type["Price"].mean(),3)
 print(unleaded98_avg)
+#Minimum
+unleaded98_min = round(df_type["Price"].min(),3)
+print(unleaded98_min)
+#Maximum
+unleaded98_max = round(df_type["Price"].max(),3)
+print(unleaded98_max)
+#Median/2nd Quartile
+unleaded98_q2 = round(df_type["Price"].median(),3)
+print(unleaded98_q2)
 
-#Diesel
+## Diesel
 df_type = df_date[df_date["Fuel Type"] == "Diesel"]
+#Mean/Average
 diesel_avg = round(df_type["Price"].mean(),3)
 print(diesel_avg)
+#Minimum
+diesel_min = round(df_type["Price"].min(),3)
+print(diesel_min)
+#Maximum
+diesel_max = round(df_type["Price"].max(),3)
+print(diesel_max)
+#Median/2nd Quartile
+diesel_q2 = round(df_type["Price"].median(),3)
+print(diesel_q2)
 
-#Heating Diesel
+## Heating Diesel
 df_type = df_date[df_date["Fuel Type"] == "Heating Diesel"]
+#Mean/Average
 heatingdiesel_avg = round(df_type["Price"].mean(),3)
 print(heatingdiesel_avg)
+#Minimum
+heatingdiesel_min = round(df_type["Price"].min(),3)
+print(heatingdiesel_min)
+#Maximum
+heatingdiesel_max = round(df_type["Price"].max(),3)
+print(heatingdiesel_max)
+#Median/2nd Quartile
+heatingdiesel_q2 = round(df_type["Price"].median(),3)
+print(heatingdiesel_q2)
 
-#Kerosene
+## Kerosene
 df_type = df_date[df_date["Fuel Type"] == "Kerosene"]
+#Mean/Average
 kerosene_avg = round(df_type["Price"].mean(),3)
 print(kerosene_avg)
+#Minimum
+kerosene_min = round(df_type["Price"].min(),3)
+print(kerosene_min)
+#Maximum
+kerosene_max = round(df_type["Price"].max(),3)
+print(kerosene_max)
+#Median/2nd Quartile
+kerosene_q2 = round(df_type["Price"].median(),3)
+print(kerosene_q2)
 
 ## Save and export the fuels prices statistics calculations
-df_stats.loc[len(df_stats)] = [today, unleaded95_avg, unleaded98_avg, diesel_avg, heatingdiesel_avg, kerosene_avg]
+df_stats.loc[len(df_stats)] = [today, unleaded95_avg, unleaded98_avg, diesel_avg, heatingdiesel_avg, kerosene_avg, unleaded95_min, unleaded98_min, diesel_min, heatingdiesel_min, kerosene_min, unleaded95_max, unleaded98_max, diesel_max, heatingdiesel_max, kerosene_max, unleaded95_q2, unleaded98_q2, diesel_q2, heatingdiesel_q2, kerosene_q2]
 df_stats.to_csv("CyFuelsPrices/Fuels_PriceStatistics.csv", index = False)
 
 ## Visualizations/Plots
@@ -88,3 +138,50 @@ plt.grid(True)
 plt.savefig('CyFuelsPrices/Fuels_Prices_Average_Evolution.png')
 plt.show()
 
+#Evolution of the daily MINIMUM fuels prices
+plt.figure(figsize=(12, 6))
+plt.plot(fuels_prices_stats['Date'], fuels_prices_stats['Unleaded95.CyprusMin'], label='Unleaded 95 - Cyprus Minimum', marker='o', color='red')
+plt.plot(fuels_prices_stats['Date'], fuels_prices_stats['Unleaded98.CyprusMin'], label='Unleaded 98 - Cyprus Minimum', marker='o', color='blue')
+plt.plot(fuels_prices_stats['Date'], fuels_prices_stats['Diesel.CyprusMin'], label='Diesel - Cyprus Minimum', marker='o', color='black')
+plt.plot(fuels_prices_stats['Date'], fuels_prices_stats['HeatingDiesel.CyprusMin'], label='Heating Diesel - Cyprus Minimum', marker='o', color='brown')
+plt.plot(fuels_prices_stats['Date'], fuels_prices_stats['Kerosene.CyprusMin'], label='Kerosene - Cyprus Minimum', marker='o', color='green')
+plt.xlabel('Date')
+plt.ylabel('Price (€/L)')
+plt.title('Evolution of the daily minimum fuels prices in Cyprus')
+plt.legend()
+plt.xticks(rotation=90)
+plt.grid(True)
+plt.savefig('CyFuelsPrices/Fuels_Prices_Minimum_Evolution.png')
+plt.show()
+
+#Evolution of the daily MAXIMUM fuels prices
+plt.figure(figsize=(12, 6))
+plt.plot(fuels_prices_stats['Date'], fuels_prices_stats['Unleaded95.CyprusMax'], label='Unleaded 95 - Cyprus Maximum', marker='o', color='red')
+plt.plot(fuels_prices_stats['Date'], fuels_prices_stats['Unleaded98.CyprusMax'], label='Unleaded 98 - Cyprus Maximum', marker='o', color='blue')
+plt.plot(fuels_prices_stats['Date'], fuels_prices_stats['Diesel.CyprusMax'], label='Diesel - Cyprus Maximum', marker='o', color='black')
+plt.plot(fuels_prices_stats['Date'], fuels_prices_stats['HeatingDiesel.CyprusMax'], label='Heating Diesel - Cyprus Maximum', marker='o', color='brown')
+plt.plot(fuels_prices_stats['Date'], fuels_prices_stats['Kerosene.CyprusMax'], label='Kerosene - Cyprus Maximum', marker='o', color='green')
+plt.xlabel('Date')
+plt.ylabel('Price (€/L)')
+plt.title('Evolution of the daily maximum fuels prices in Cyprus')
+plt.legend()
+plt.xticks(rotation=90)
+plt.grid(True)
+plt.savefig('CyFuelsPrices/Fuels_Prices_Maximum_Evolution.png')
+plt.show()
+
+#Evolution of the daily MEDIAN fuels prices
+plt.figure(figsize=(12, 6))
+plt.plot(fuels_prices_stats['Date'], fuels_prices_stats['Unleaded95.CyprusMedian'], label='Unleaded 95 - Cyprus Median', marker='o', color='red')
+plt.plot(fuels_prices_stats['Date'], fuels_prices_stats['Unleaded98.CyprusMedian'], label='Unleaded 98 - Cyprus Median', marker='o', color='blue')
+plt.plot(fuels_prices_stats['Date'], fuels_prices_stats['Diesel.CyprusMedian'], label='Diesel - Cyprus Median', marker='o', color='black')
+plt.plot(fuels_prices_stats['Date'], fuels_prices_stats['HeatingDiesel.CyprusMedian'], label='Heating Diesel - Cyprus Median', marker='o', color='brown')
+plt.plot(fuels_prices_stats['Date'], fuels_prices_stats['Kerosene.CyprusMedian'], label='Kerosene - Cyprus Median', marker='o', color='green')
+plt.xlabel('Date')
+plt.ylabel('Price (€/L)')
+plt.title('Evolution of the daily median fuels prices in Cyprus')
+plt.legend()
+plt.xticks(rotation=90)
+plt.grid(True)
+plt.savefig('CyFuelsPrices/Fuels_Prices_Median_Evolution.png')
+plt.show()
