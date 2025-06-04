f03cd06834d6e86653a0783b00ebdd128a9b9f4d
kvitalis <157591960+kvitalis@users.noreply.github.com>
2025-06-04 14:49:52 +0300

    Update SecondIteration.py

--- PythonCodes/SecondIteration.py
+++ PythonCodes/SecondIteration.py
@@ -3189,28 +3189,18 @@ new_row.append("RESTAURANTS AND HOTELS")
 new_row.append("Meze Tavern") 
 list_.loc[len(list_)] = new_row
 list_['Name'] = list_['Name'].apply(lambda x:x)
+'''
 
 #Stock Center - The Used Car Experts (https://www.stock-center.com.cy/el/searchresults/?cg=&mk=&md=&yf=2000&yt=2024&km=0&cf=0&ct=1600&et=&pf=0&pt=15000&mp=0&ar=#page_1)
 new_row=[]
 new_row.append(datetime.today().strftime("%Y-%m-%d"))
-new_row.append("KIA RIO 1.2cc MANUAL PETROL 1200cc")
-new_row.append(float(12700))
+new_row.append("VOLKSWAGEN GOLF 1.5cc HIGHLINE AUTO PETROL 1500cc")
+new_row.append(float(13000))
 new_row.append("Second-hand motor cars")
 new_row.append("TRANSPORT")
 new_row.append("Stock Center") 
 list_.loc[len(list_)] = new_row
 list_['Name'] = list_['Name'].apply(lambda x:x)
-
-new_row=[]
-new_row.append(datetime.today().strftime("%Y-%m-%d"))
-new_row.append("KIA PICANTO 1.0cc AUTOMATIC PETROL 1000cc")
-new_row.append(float(11300))
-new_row.append("Second-hand motor cars")
-new_row.append("TRANSPORT")
-new_row.append("Stock Center") 
-list_.loc[len(list_)] = new_row
-list_['Name'] = list_['Name'].apply(lambda x:x)
-'''
 
 #Primetel (https://primetel.com.cy/giga-unlimited --> https://primetel.com.cy/giga-unlimited-summer-offer) 
 new_row=[]
