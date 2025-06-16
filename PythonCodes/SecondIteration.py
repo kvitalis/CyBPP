d075b5899d9df2b1b332efbd005ff8e58af4eabc
kmicha714 <159122641+kmicha714@users.noreply.github.com>
2025-06-16 14:04:15 +0300

    Update SecondIteration.py

--- PythonCodes/SecondIteration.py
+++ PythonCodes/SecondIteration.py
@@ -40,10 +40,12 @@ list_ = pd.DataFrame(columns = ["Date","Name","Price","Subclass","Division","Ret
 
 def results_supermarketcy(u):
     
-    header = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'}
+    header = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'} #1st Try
+    header = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',} #2nd Try
     url_new = "https://www.supermarketcy.com.cy/" + str(Item_url_)
-    bs = BeautifulSoup(url_new, "html.parser")
-    response = requests.get(bs,{'headers':header})
+    #bs = BeautifulSoup(url_new, "html.parser") #1st Try
+    #response = requests.get(bs,{'headers':header}) # 1St Try
+    response = requests.get(url_new, headers=header) #2nd Try
            
     if (response.status_code != 200) or ("Η σελίδα δεν βρέθηκε" in response.text) or ("Η σελίδα αφαιρέθηκε" in response.text):
         website_false.append(name_)
