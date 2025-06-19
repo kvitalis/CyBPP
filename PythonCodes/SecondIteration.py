8c61b2446d38f6ac8079cc1000a6553a62f45ded
kvitalis <157591960+kvitalis@users.noreply.github.com>
2025-06-19 10:51:30 +0300

    Update SecondIteration.py

--- PythonCodes/SecondIteration.py
+++ PythonCodes/SecondIteration.py
@@ -47,13 +47,13 @@ def results_supermarketcy(u):
     #response = requests.get(bs)
     
     ## 2nd way (*it's NOT working*)
-    header = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'}
-    bs = BeautifulSoup(url_new, "html.parser")
-    response = requests.get(bs, {'headers': header})
+    #header = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'}
+    #bs = BeautifulSoup(url_new, "html.parser")
+    #response = requests.get(bs, {'headers': header})
     
     ## 3rd way (*it's NOT working*)
-    #header = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'}
-    #response = requests.get(url_new, headers = header) 
+    header = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'}
+    response = requests.get(url_new, headers = header) 
 
     if (response.status_code != 200): #or ("Η σελίδα δεν βρέθηκε" in response.text) or ("Η σελίδα αφαιρέθηκε" in response.text):
         website_false.append(name_)
