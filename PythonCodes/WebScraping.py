78f9194aac2fb694d98bde53a9689c177c70be72
kmicha714 <159122641+kmicha714@users.noreply.github.com>
2025-07-01 15:51:37 +0300

    Update WebScraping.py

--- PythonCodes/WebScraping.py
+++ PythonCodes/WebScraping.py
@@ -58,7 +58,6 @@ def results_supermarketcy(u):
     header = {
         "authority": "www.supermarketcy.com.cy",
         "method": "GET",
-        "path": "/sifounas-olikis-psomi-kommeno-780g",
         "scheme": "https",
         "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
         "accept-encoding": "gzip, deflate, br, zstd",
