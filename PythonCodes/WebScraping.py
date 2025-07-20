d06b4dd530ad7f7602a48f274b8d162ffcaae0c5
kvitalis <157591960+kvitalis@users.noreply.github.com>
2025-07-20 12:27:52 +0300

    Update WebScraping.py

--- PythonCodes/WebScraping.py
+++ PythonCodes/WebScraping.py
@@ -1013,6 +1013,8 @@ def results_ewholesale(u):
         soup = BeautifulSoup(response.content, "html.parser")
         element_soup = soup.find_all("div",{"class":"hM4gpp"}) 
         price_= element_soup[0].text.replace("€Τιμή","").replace(" ","").replace(",",".").replace("€","")
+        print(price_)
+        
         new_row.append(datetime.now().strftime('%Y-%m-%d'))
         new_row.append(name_)
         new_row.append(float(price_))
