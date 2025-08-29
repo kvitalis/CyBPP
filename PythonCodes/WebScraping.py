38748f9cc53d47d9b7014516988738ccb90fd06a
kvitalis <157591960+kvitalis@users.noreply.github.com>
2025-08-29 11:38:22 +0300

    Update WebScraping.py

--- PythonCodes/WebScraping.py
+++ PythonCodes/WebScraping.py
@@ -330,11 +330,19 @@ def results_ikea(u):
     #header = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'}
     #response = requests.get(Item_url_, headers=header)
 
-    if response.status_code == 200 :
+    if (response.status_code != 200) :
+        website_false.append(name_)
+        website_false.append(subclass_)
+        website_false.append(Item_url_)
+        website_false.append(division_)
+        website_false.append(retailer_)
+        daily_errors.loc[len(daily_errors)] = website_false
+        daily_errors["Name"] = daily_errors["Name"].apply(lambda x:x) 
+    else:
         soup = BeautifulSoup(response.content, "html.parser")
         #soup = BeautifulSoup(response.text, "html.parser")
-        element_soup = soup.find_all("span", {"class":"price__sr-text"})
-        price_ = element_soup[0].text.strip("Τρέχουσα τιμή € ").replace("Αρχική τιμή € ","").replace(",",".")
+        element_soup = soup.find_all("span",{"class":"price__sr-text"})
+        price_ = element_soup[0].text.strip("Τρέχουσα τιμή € ").replace(",",".")
         print(price_)
         new_row.append(datetime.now().strftime('%Y-%m-%d'))
         new_row.append(name_)
@@ -344,14 +352,6 @@ def results_ikea(u):
         new_row.append("IKEA")
         list_.loc[len(list_)] = new_row
         list_['Name'] = list_['Name'].apply(lambda x:x) 
-    else:
-        website_false.append(name_)
-        website_false.append(subclass_)
-        website_false.append(Item_url_)
-        website_false.append(division_)
-        website_false.append(retailer_)
-        daily_errors.loc[len(daily_errors)] = website_false
-        daily_errors["Name"] = daily_errors["Name"].apply(lambda x:x)
                      
 def results_stephanis(u):
 
