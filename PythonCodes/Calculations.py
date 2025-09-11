dd4fe45776d7b12250f34354f8ce509da4b4d3ee
kvitalis <157591960+kvitalis@users.noreply.github.com>
2025-09-11 12:51:12 +0300

    Update Calculations.py

--- PythonCodes/Calculations.py
+++ PythonCodes/Calculations.py
@@ -8,7 +8,7 @@ from datetime import datetime, timedelta
 
 # Ignore specific warning
 warnings.simplefilter("ignore")
-
+'''
 today = datetime.today().strftime("%Y-%m-%d")
 #today = '2025-07-31'
 
@@ -284,8 +284,8 @@ else:
 # If you want to re-calculate everything between two specific dates, then run the following while-loop :
 ########################################################################################################################################################################################
 
-start_date = datetime.strptime("2025-09-02", "%Y-%m-%d")
-end_date = datetime.strptime("2025-09-09", "%Y-%m-%d")
+start_date = datetime.strptime("2025-09-01", "%Y-%m-%d")
+end_date = datetime.strptime("2025-09-11", "%Y-%m-%d")
 
 today_p = start_date
 
@@ -558,4 +558,3 @@ while today_p <= end_date:
         pass
     today_p += timedelta(days=1)
 ####################################################    End of while-loop    ################################################################################################################
-'''
