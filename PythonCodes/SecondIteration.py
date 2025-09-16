a520ec8d3f68bd57b36b35e1f5ab70fcf657ed85
kvitalis <157591960+kvitalis@users.noreply.github.com>
2025-09-16 16:42:39 +0300

    Update SecondIteration.py

--- PythonCodes/SecondIteration.py
+++ PythonCodes/SecondIteration.py
@@ -751,12 +751,12 @@ def results_cablenet(u):
     #bs = BeautifulSoup(Item_url_, "html.parser")
     #response = requests.get(bs) 
     ## 2 
-    response = requests.get(Item_url_)
+    #response = requests.get(Item_url_)
 
     ### with headers 
     header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36'}
     ## 1 
-    #response = requests.get(Item_url_, headers = header)
+    response = requests.get(Item_url_, headers = header)
     ## 2 
     #bs = BeautifulSoup(Item_url_, "html.parser")
     #response = requests.get(bs, {'headers': header})
