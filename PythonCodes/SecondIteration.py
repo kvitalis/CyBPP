e641535c5253446f594fdf3d712ac0292e8eef52
kvitalis <157591960+kvitalis@users.noreply.github.com>
2025-09-29 13:40:55 +0300

    Update SecondIteration.py

--- PythonCodes/SecondIteration.py
+++ PythonCodes/SecondIteration.py
@@ -1711,10 +1711,10 @@ def results_wolt(u):
     header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36'}
     #header = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'}
     ## 1 
-    bs = BeautifulSoup(Item_url_, "html.parser")
-    response = requests.get(bs, {'headers':header})
+    #bs = BeautifulSoup(Item_url_, "html.parser")
+    #response = requests.get(bs, {'headers':header})
     ## 2
-    #response = requests.get(Item_url_, headers = header) 
+    response = requests.get(Item_url_, headers = header) 
     ## 3 
     #with httpx.Client(headers = header) as client:
     #    response = client.get(Item_url_)
