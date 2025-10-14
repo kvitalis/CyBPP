68de2e7dd15ba63b2def7edd10b046221ad11754
kvitalis <157591960+kvitalis@users.noreply.github.com>
2025-10-14 11:13:19 +0300

    Update CyStat.py

--- CyStat/CyStat.py
+++ CyStat/CyStat.py
@@ -346,7 +346,7 @@ def cystat(last_results):
            plt.show()
 
 def is_first_thursday(date):
-    date = '2025-10-02'
+    #date = '2025-10-02'
     date = datetime.strptime(date, "%Y-%m-%d")
     weekday = date.weekday()
     if weekday == 3 and date.month != (date - timedelta(days=7)).month:
