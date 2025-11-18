55104896878ffc8a8b220f3b3ec7c73031a7c17d
kvitalis <157591960+kvitalis@users.noreply.github.com>
2025-11-18 09:13:12 +0200

    Update WebScraping.py

--- PythonCodes/WebScraping.py
+++ PythonCodes/WebScraping.py
@@ -3270,8 +3270,8 @@ for u in range(0, len(urls)):
         results_bwell_pharmacy(u)
     elif retailer_=="Novella":
         results_novella(u) 
-    elif retailer_=="Evdokia Jewellery": #*deactivated in 14/11/25 because its webpage isn't working (response=500)*
-        results_evdokia_jewellery(u)
+    #elif retailer_=="Evdokia Jewellery": #*deactivated in 14/11/25 because its webpage isn't working (response=500)*
+    #    results_evdokia_jewellery(u)
     elif retailer_=="LensesCY":
         results_lensescy(u)    
     elif retailer_=="Centroptical":
